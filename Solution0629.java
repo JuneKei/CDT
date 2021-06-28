3eea8dc68453333088b71118bdce7593d35b7f3c
JuneKei <85489867+JuneKei@users.noreply.github.com>
2021-06-29 08:16:12 +0900

    하샤드 수
    문제 설명
    양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
    
    제한 조건
    x는 1 이상, 10000 이하인 정수입니다.

---
+++ Solution0629.java
@@ -0,0 +1,21 @@
+package codingtest;
+
+public class Solution0629 {
+    public boolean solution(int x) {
+    	int h5=x/10000;
+	    int h4=(x/1000)%10;
+	    int h3=(x/100)%10;
+	    int h2=(x/10)%10;
+	    int h1=(x/1)%10; 
+    	// %10을 해야지 값의 1의자리까지 됨
+	    // h4도 %10 해야 값이 10000일때 0이 나옴
+	    
+	    boolean answer = false;
+	    
+	    if((x%(h5+h4+h3+h2+h1))==0) {
+	    	answer = true;
+	    }
+
+        return answer;
+    }
+}
