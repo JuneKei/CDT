e5758a41b71728c59bc07ccec4473fb64312181b
JuneKei <85489867+JuneKei@users.noreply.github.com>
2021-07-03 16:53:09 +0900

    Update Solution0702.java

--- Solution0702.java
+++ Solution0702.java
@@ -3,7 +3,7 @@ package codingtest;
 public class Solution0702 {
     public String solution(int num) {
         String answer = (num%2==0) ? "Even" : "Odd";        
-       
+        // 삼항연산자 사용해서 짝수면 Even
         return answer;
     }
 }
