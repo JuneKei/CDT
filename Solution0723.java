cbe27dd776ea52fc8766b92b993dfdfd2f83d416
JuneKei <85489867+JuneKei@users.noreply.github.com>
2021-07-23 07:40:06 +0900

    예산
    Summer/Winter Coding(~2018)

---
+++ Solution0723.java
@@ -0,0 +1,17 @@
+package july;
+
+import java.util.Arrays;
+public class Solution0723 {
+    public int solution(int[] d, int budget) {
+        int answer =0;
+	    Arrays.sort(d);
+        
+        for(int i=0 ;  budget>=d[i] ;i++){
+		budget -= d[i];
+		answer++;
+		if(i==d.length-1) break;
+	}
+        
+        return answer;
+    }
+}
