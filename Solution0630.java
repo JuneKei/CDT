c717d8241bc8fd472aabcc9357ff5edd68799540
JuneKei <85489867+JuneKei@users.noreply.github.com>
2021-09-07 06:42:28 +0900

    콜라츠 추측

--- Solution0630.java
+++ Solution0630.java
@@ -19,3 +19,16 @@ public class Solution0630 {
 	        return cnt;
     }
 }
+
+def solution(n):
+    cnt=0
+    while n!=1:
+        if n%2==0:
+            n=n/2
+        else:
+            n=n*3+1
+        cnt+=1
+        if cnt>=500:
+            cnt=-1
+            break
+    return cnt
